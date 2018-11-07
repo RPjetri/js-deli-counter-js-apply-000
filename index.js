389fecf1906bcce2bd342c376f1a11d5269b44ee
Robert Pjetri <rpjetri@hotmail.com>
2018-11-07 22:57:39 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,16 +1,25 @@
+var ticketNumber = []
+
+
 function takeANumber(line, name) {
+  for (n = 0; n > line.length; n++) {  
+  ticketNumber.push(n)
+  return `You are ticket number ${ticketNumber[n]}`
+}
   line.push(name)
   return `Welcome, ${name}. You are number ${line.length} in line.`
 }
 
+
 function nowServing(katzDeliLine) {
   if (katzDeliLine.length > 1) {
     return `Currently serving ${katzDeliLine.shift()}.`
-  }
+}
   else if (katzDeliLine.length === 0) {
     return `There is nobody waiting to be served!`
   }
 }
+
 
 function currentLine(line) {
   if (line.length === 0) {
